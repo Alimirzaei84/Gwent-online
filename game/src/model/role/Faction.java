386230dd58d5b7058dc85c8fdfa23c6784d1708c
Erfan Bohloul <erfanbohloul@gmail.com>
2024-05-28 14:31:49 +0330

    merging master branch into main and then remove the game folder becouse it was duplicated

--- game/src/model/role/Faction.java
+++
@@ -1,9 +0,0 @@
-package model.role;
-
-public enum Faction {
-    NORTHERN_REALMS,
-    NILFGAARDIAN_EMPIRE,
-    MONSTERS,
-    SCOIA_TEAL,
-    SKELLIGE
-}
--- game/src/model/role/Special.java
+++
@@ -1,4 +0,0 @@
-package model.role;
-
-public class Special {
-}
